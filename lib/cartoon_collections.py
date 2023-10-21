3fb0a745af46fd90c561873fef2ad92f26def56a
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-20 23:46:20 -0600

    this shows how to use loops and if statements and list comprehensions

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,27 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i in range(len(names)):
+        print(f"{(i + 1)}. {names[i]}");
+    return None;
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(mcalls):
+    if (mcalls == None): return None;
+    elif (len(mcalls) < 1): return [];
+    else: return [f"{mc.capitalize()}!" for mc in mcalls];
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(mcalls):
+    if (mcalls == None or len(mcalls) < 1): return False;
+    else: pass;
+    for mc in mcalls:
+        if (len(mc) > 4): return True;
+        else: pass;
+    return False;
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    if (items == None or len(items) < 1): return None;
+    else: pass;
+    cheeses = ["cheddar", "gouda", "camembert"];
+    for item in items:
+        for cheese in cheeses:
+            if (item == cheese): return item;
+            else: pass;
+    return None;
